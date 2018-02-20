aa6b44b258cddecb687fb06c44d16d5f9cd6332a
cionman <topspin1278@naver.com>
2018-02-20 10:25:15 +0900

    - 동기부분, 비동기부분 실험

--- publisher.py
+++ publisher.py
@@ -2,10 +2,10 @@ import time
 
 import tasks
 
-result = tasks.add.delay(3, 9)
+result = tasks.add.delay(3, 9) # 여기는 비동기
 print('여기는 바로 찍히나?')
-print('결과 : ' , result.get())
-
+print('결과 : ' , result.get()) # 여기는 동기 
+print('여기는???')
 time.sleep(15)
 
 print('결과 15초 후 : ', result.get())
